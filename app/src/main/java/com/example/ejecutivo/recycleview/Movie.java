26f1fd799b422347a8570b07a3d447fee6936ff4
leonardo.aguilar <leonardo.aguilar@sferea.com>
2017-08-03 14:05:17 -0500

    Added files

---
+++ app/src/main/java/com/example/ejecutivo/recycleview/Movie.java
@@ -0,0 +1,42 @@
+package com.example.ejecutivo.recycleview;
+
+/**
+ * Created by ejecutivo on 29/05/2017.
+ */
+
+public class Movie {
+    private String  title, genre, year;
+
+    public Movie() {
+    }
+
+    public Movie(String title, String genre, String year) {
+        this.title = title;
+        this.genre = genre;
+        this.year = year;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getYear() {
+        return year;
+    }
+}
---
+++ app/src/main/java/com/example/ejecutivo/recycleview/MoviesAdapter.java
@@ -0,0 +1,53 @@
+package com.example.ejecutivo.recycleview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by ejecutivo on 29/05/2017.
+ */
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHolder> {
+    private List<Movie> movieList;
+
+    public class MyViewHolder extends  RecyclerView.ViewHolder{
+        public TextView title, year, genre;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            genre = (TextView) view.findViewById(R.id.genre);
+            year = (TextView) view.findViewById(R.id.year);
+        }
+    }
+
+    public MoviesAdapter(List<Movie> movieList) {
+        this.movieList = movieList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.movie_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position){
+        Movie movie = movieList.get(position);
+        holder.title.setText(movie.getTitle());
+        holder.genre.setText(movie.getGenre());
+        holder.year.setText(movie.getYear());
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size();
+    }
+ }
